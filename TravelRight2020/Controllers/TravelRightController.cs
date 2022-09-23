3e5276c3ce8c5b696c7010396da22f62c9ac9991
KriteshKandolkar <kandolkarkritesh@gmail.com>
2022-09-23 12:37:25 +0530

    Initialise Code

---
+++ TravelRight2020/Controllers/TravelRightController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelRight2020.Controllers
+{
+    public class TravelRightController : Controller
+    {
+        // GET: TravelRight
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ TravelRight2020/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TravelRight2020.MvcApplication" Language="C#" %>
